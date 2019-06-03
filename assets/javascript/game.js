fe38a016d9fe2b79914906bb87f44b483e395f3e
ckaoki <cullan_aoki@msn.com>
2019-06-02 19:31:04 -0700

    completed basic layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// TODO: generate random number from 19-120 to match 
+// TODO: generate random numbers from 1-12 for four crystals 
+$(document).ready(function(){
+
+// var newentry = $("<div>");
+// newentry = "abcd";
+// $("body").append(newentry);
+});
