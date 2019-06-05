d75337d1cd5c836468413c3bd714a6117a314041
ckaoki <cullan_aoki@msn.com>
2019-06-04 18:07:18 -0700

    initial functional game. Included win/loss results and dynamically formatted message. Completed readme.md file.

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # unit-4-game
+This project contains a random number guessing game. Its imperative that you have a good memory and can add :). This pages was designed using Bootstrap and are responsive.
+
+Overview
+1. You will be given a random number from 19 to 120 at the start of the game.
+
+2. There are four crystals. By clicking on a crystal you will add a specific amount of points (1 to 12) to your total score.
+
+3. You win the game by matching your total score to a random number, you lose the game if your total score goes above the random number.
+
+4. The value of each crystal is hidden from you until you clock on it.
+
+5. Each time when the game starts, the game will change the values of each crystal. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+
+### Prerequisites
+
+Browser minimum requirements:
+* Chrome ver 74
+* Firefox ver 60
+
+### Installing
+
+To view files navigate to  https://ckaoki.github.io/unit-4-game/.
+
+To install source files use git to clone files from https://github.com/ckaoki/unit-4-game.
+
+## Running the tests
+
+Open files in browser and make sure they are properly formatted. Your eyes are the test.
+
+### Break down into end to end tests
+
+Source files have been extensively tested by displaying pages in Google Chrome and using visual verification.
+
+### And coding style tests
+
+I'm starting to get some style.
+
+## Deployment
+
+Navigate to https://github.com/ckaoki/unit-4-game to clone.
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/)
+
+## Contributing
+
+Please read our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use github for version control (https://github.com/your/project/tags). 
+
+## In development
+Implementing textual hints.
+
+## Authors
+
+* **Cullan Aoki** - *Initial work* - https://github.com/ckaoki/unit-4-game
+
+## License
+
+This project is not licensed.
+
+## Acknowledgments
+
+* Joe Rehfuss
+* Trae Shanks
+* Lan Truong
+* James Hanley
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,66 @@
-// TODO: generate random number from 19-120 to match 
-function genRandomNum(){
-    return Math.floor((Math.random()*102))+19;
+var targetNumber = 0;
+var score = 0;
+var wins = 0;
+var losses = 0;
+var crystalValues = [];
+const numCrystals = 4;
+
+function init(){
+    // initialize GUI
+    score=0;
+    $("#score").text(0);
+    genRandomTargetNum();
+    genRandomCrystalNums();
 }
 
-// TODO: generate random numbers from 1-12 for four crystals
-var allImages = $( "img" );
-function genCrystalsRandomNums(){
-    
-    var crystalIDs = $('img.imgCrystal');
-    for(var i=0; i<crystalIDs.length; i++){
-        var randomNum = Math.ceil(Math.random()*12);
-        $(crystalIDs[i]).attr({value: randomNum});
-    }
-        //TODO: Delete this
-        //   $("img.imgCrystal").find(allImages);
-        //   console.log($("img").attr("value"));
-        // const crystalValues = $('img.imgCrystal').map((i, el) => el.getAttribute('value')).get();
-        // const ids = $('img.imgCrystal').map((i, el) => el.getAttribute('id'));
-        // console.log(crystalValues);
-        // console.log(crystalValues[3]);
-        // console.log(ids2[1].attr({value: 99}));
-        // $(crystalIDs[1]).attr({value: 99});
-        // console.log(ids2[2].getAttribute("value"));
-        // $("#crystal_1").attr({value: 7});      
-    }
-
-
-function myAlert(currentCrystal){
-    $(currentCrystal).attr("value");
-    console.log(genRandomNum());
-
-
+// generate random number from 19-120 to match 
+function genRandomTargetNum(){
+    targetNumber = Math.floor((Math.random()*102))+19;
+    $("#targetNumber").text(targetNumber);
 }
+
+// generate random numbers from 1-12 for four crystals
+function genRandomCrystalNums(){       
+    for(var i=0; i<numCrystals; i++){
+        crystalValues[i] = Math.ceil(Math.random()*12);
+    }
+    console.log(crystalValues);
+}
+
+// crystal was clicked, play game.
+function playGame(crystalIndex){
+    // add crystal value to score
+    score += crystalValues[crystalIndex];
+    $("#score").text(score);
+
+    // verify score
+    if (score === targetNumber){
+        // you win
+        wins++;
+        $("#wins").text(wins);
+        $("#gameResult").css({"color":"green", "font-weight": "bold", "font-size": "24pt"});
+        $("#gameResult").text("You Won!")
+        init();
+    }
+    else if (score > targetNumber){
+        //you lose
+        losses++;
+        $("#losses").text(losses);
+        $("#gameResult").text("You Lost!")
+        $("#gameResult").css({"color":"red", "font-weight": "bold", "font-size": "24pt"});
+        init();
+    }
+    else{
+        // still going.  Clear results
+        $("#gameResult").text("");
+    }
+}
+
 
 $(document).ready(function(){
-    genCrystalsRandomNums();
-    $(".imgCrystal").click(function(){
-        myAlert(this);
-    });
+    init();
 
-// var newentry = $("<div>");
-// newentry = "abcd";
-// $("body").append(newentry);
+    $(".crystalImg").click(function(){
+       playGame($(this).attr("index"));
+    });
 });
